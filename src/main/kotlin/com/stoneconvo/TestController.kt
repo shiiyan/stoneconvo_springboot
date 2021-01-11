d07882f2429a8da208a981bc21e0f3f1ecf89ddf
shiiyan <yan@softbank126072069094.bbtec.net>
2021-01-11 22:08:00 +0900

    create test controller

---
+++ src/main/kotlin/com/stoneconvo/TestController.kt
@@ -0,0 +1,12 @@
+package com.stoneconvo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+    @GetMapping("/")
+    fun test() = "Hello from SpringBoot"
+}
