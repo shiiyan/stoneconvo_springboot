9c9bdd1c0da5b9d12463978ff5c3f0f7b7b360bf
yan <yan39123@gmail.com>
2021-01-25 09:10:24 +0900

    impl administrator repo

---
+++ src/main/kotlin/com/stoneconvo/repository/administrator/JooqAdministratorRepository.kt
@@ -0,0 +1,18 @@
+package com.stoneconvo.repository.administrator
+
+import com.stoneconvo.codegen.tables.daos.JAdministratorsDao
+import com.stoneconvo.domain.administrator.Administrator
+import com.stoneconvo.domain.userAccount.UserAccountId
+import org.springframework.stereotype.Repository
+
+@Repository
+class JooqAdministratorRepository : AdministratorRepository {
+    private val dao = JAdministratorsDao()
+
+    override fun findByUserId(userId: UserAccountId): Administrator? =
+        dao.fetchOneByJUserAccountId(userId.id)?.let {
+            Administrator(
+                UserAccountId(it.userAccountId)
+            )
+        }
+}
