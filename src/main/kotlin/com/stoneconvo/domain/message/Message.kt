7bbe7f0ab34a2f30951c331603f7f178d96ca276
yan <yan39123@gmail.com>
2021-02-03 09:11:31 +0900

    create message application service

--- src/main/kotlin/com/stoneconvo/domain/message/Message.kt
+++ src/main/kotlin/com/stoneconvo/domain/message/Message.kt
@@ -1,14 +1,30 @@
 package com.stoneconvo.domain.message
 
+import com.stoneconvo.domain.chatroom.ChatRoomId
 import com.stoneconvo.domain.userAccount.UserAccountId
 import java.time.LocalDateTime
 
 class Message(
     val id: MessageId,
     var content: MessageContent,
+    val roomId: ChatRoomId,
+    val senderId: UserAccountId,
     val sentDateTime: LocalDateTime,
-    val sender: UserAccountId
 ) {
+    companion object {
+        fun create(
+            content: MessageContent,
+            roomId: ChatRoomId,
+            senderId: UserAccountId
+        ) = Message(
+            id = MessageId.create(),
+            content = content,
+            roomId = roomId,
+            senderId = senderId,
+            sentDateTime = LocalDateTime.now()
+        )
+    }
+
     fun updateContent(newMessageContent: MessageContent) {
         content = newMessageContent
     }
