4720af7c9dc42e4471db9bc7d794d7ac39d981fa
yan <yan39123@gmail.com>
2021-03-09 22:23:55 +0900

    test cookie and add application properties

--- src/main/kotlin/com/stoneconvo/TestController.kt
+++ src/main/kotlin/com/stoneconvo/TestController.kt
@@ -1,12 +1,25 @@
 package com.stoneconvo
 
+import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 @RequestMapping("/test")
 class TestController {
     @GetMapping("/")
-    fun test() = "Hello from SpringBoot"
+    fun test(@CookieValue("test-cookie") testCookie: String) =
+        "Hello from SpringBoot with testCookie $testCookie"
+
+    @GetMapping("/new_cookie")
+    fun createNewCookie(response: HttpServletResponse): String {
+        val newCookie = Cookie("test-cookie", "I'm_new_test_cookie")
+
+        response.addCookie(newCookie)
+
+        return "new cookie is set"
+    }
 }
