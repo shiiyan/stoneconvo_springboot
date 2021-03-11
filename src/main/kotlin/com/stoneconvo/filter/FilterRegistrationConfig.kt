0981733dc2edffebfda3f49b69e7995b8893149c
yan <yan39123@gmail.com>
2021-03-11 09:36:08 +0900

    use request logging filter

--- src/main/kotlin/com/stoneconvo/filter/FilterRegistrationConfig.kt
+++ src/main/kotlin/com/stoneconvo/filter/FilterRegistrationConfig.kt
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class FilterRegistrationConfig {
     @Bean
-    fun logFilter(): FilterRegistrationBean<LogFilter> {
-        val registrationBean = FilterRegistrationBean<LogFilter>()
-        registrationBean.filter = LogFilter()
+    fun logFilter(): FilterRegistrationBean<RequestLoggingFilter> {
+        val registrationBean = FilterRegistrationBean<RequestLoggingFilter>()
+        registrationBean.filter = RequestLoggingFilter()
         registrationBean.addUrlPatterns("*")
         return registrationBean
     }
