d15b3b692506402c2f288f28ede2e29b8ee5e576
yan <yan39123@gmail.com>
2021-03-16 09:56:29 +0900

    create request auth filter

--- src/main/kotlin/com/stoneconvo/filter/FilterRegistrationConfig.kt
+++ src/main/kotlin/com/stoneconvo/filter/FilterRegistrationConfig.kt
@@ -7,10 +7,8 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class FilterRegistrationConfig {
     @Bean
-    fun logFilter(): FilterRegistrationBean<RequestLoggingFilter> {
-        val registrationBean = FilterRegistrationBean<RequestLoggingFilter>()
-        registrationBean.filter = RequestLoggingFilter()
-        registrationBean.addUrlPatterns("*")
-        return registrationBean
-    }
+    fun logFilter(): FilterRegistrationBean<RequestLoggingFilter> = FilterRegistrationBean(RequestLoggingFilter())
+
+    @Bean
+    fun authFilter(): FilterRegistrationBean<RequestAuthFilter> = FilterRegistrationBean(RequestAuthFilter())
 }
---
+++ src/main/kotlin/com/stoneconvo/filter/RequestAuthFilter.kt
@@ -0,0 +1,34 @@
+package com.stoneconvo.filter
+
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Order(1)
+class RequestAuthFilter : OncePerRequestFilter() {
+    companion object {
+        private const val PATTERN = "^[A-Za-z0-9]{20}$"
+    }
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        if (!hasUserIdInCookies(request)) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "Request Unauthorized")
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean = "/login" == (request.requestURI)
+
+    private fun hasUserIdInCookies(request: HttpServletRequest) =
+        request.cookies
+            ?.any { cookie -> cookie.name == "user-id" && PATTERN.toRegex().matches(cookie.value) }
+            ?: false
+}
