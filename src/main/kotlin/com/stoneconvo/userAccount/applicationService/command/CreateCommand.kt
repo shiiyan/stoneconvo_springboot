4b22332129650eda68e3a4e66b1f95e8983014ed
yan <yan39123@gmail.com>
2021-03-30 10:06:42 +0900

    add generate hash helper

--- src/main/kotlin/com/stoneconvo/userAccount/applicationService/command/CreateCommand.kt
+++ src/main/kotlin/com/stoneconvo/userAccount/applicationService/command/CreateCommand.kt
@@ -1,6 +1,7 @@
 package com.stoneconvo.userAccount.applicationService.command
 
 import com.stoneconvo.common.domain.UserAccountId
+import com.stoneconvo.common.helper.Helper
 import com.stoneconvo.userAccount.domain.PasswordHash
 import com.stoneconvo.userAccount.domain.UserAccountName
 
@@ -8,4 +9,16 @@ data class CreateCommand(
     val currentUserId: UserAccountId,
     val name: UserAccountName,
     val passwordHash: PasswordHash
-)
+) {
+    companion object {
+        fun create(
+            currentUserId: String,
+            name: String,
+            password: String
+        ) = CreateCommand(
+            currentUserId = UserAccountId(currentUserId),
+            name = UserAccountName(name),
+            passwordHash = PasswordHash(Helper.generateHash(password))
+        )
+    }
+}
