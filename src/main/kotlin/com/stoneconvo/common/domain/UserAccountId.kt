a74cbffbe64cf21a4b9a3553fb30d19f5154f1a8
yan <yan39123@gmail.com>
2021-04-01 09:48:16 +0900

    fix regex pattern and update initial migration script

--- src/main/kotlin/com/stoneconvo/common/domain/UserAccountId.kt
+++ src/main/kotlin/com/stoneconvo/common/domain/UserAccountId.kt
@@ -5,7 +5,7 @@ import com.stoneconvo.common.helper.Helper
 data class UserAccountId(val value: String) : ValueObject() {
     companion object {
         fun create() = UserAccountId(value = Helper.generateRandomId())
-        private const val PATTERN = "^[A-Za-z0-9]{20}$"
+        private const val PATTERN = "^[A-Za-z0-9]{12}$"
     }
 
     init {
